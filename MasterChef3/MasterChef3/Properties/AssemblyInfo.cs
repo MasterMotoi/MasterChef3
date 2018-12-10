7910cc632bd99a673ada14fcea16a7c8ef3760aa
eliatib <thibault.michel@viacesi.fr>
2018-12-10 10:54:23 +0100

    Commit obligatoire pour création brach TH

--- MasterChef3/MasterChef3/Properties/AssemblyInfo.cs
+++ MasterChef3/MasterChef3/Properties/AssemblyInfo.cs
@@ -34,3 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("MasterChef3.Tests")]
+[assembly: InternalsVisibleTo("MasterChef3.Explorables")]
+
