652da2d8d7e5f2bbcbc09e498e3b68aa15fdd1d6
vincesilyon <45558503+vincesilyon@users.noreply.github.com>
2018-12-10 17:47:37 +0100

    r
    r

---
+++ LIVRABLES FINAUX/Dossier_MasterChef3/BDD/masterchef3.materiel_lavable.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [masterchef3].[materiel_lavable] (
+    [id_materiel_lavable] INT           IDENTITY (1, 1) NOT NULL,
+    [machine]             NVARCHAR (50) NOT NULL,
+    [propre]              SMALLINT      NOT NULL,
+    [id_materiel]         INT           NOT NULL,
+    CONSTRAINT [PK_materiel_lavable_id_materiel_lavable] PRIMARY KEY CLUSTERED ([id_materiel_lavable] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_SSMA_SOURCE', @value = N'masterchef3.materiel_lavable', @level0type = N'SCHEMA', @level0name = N'masterchef3', @level1type = N'TABLE', @level1name = N'materiel_lavable';
+
