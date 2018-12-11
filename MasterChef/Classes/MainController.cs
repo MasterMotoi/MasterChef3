458036a1cf0f96e991012906745d419a0a07075c
Louis Corbet <ls.corbet@gmail.com>
2018-12-11 11:22:20 +0100

    creation du maincontroller

---
+++ MasterChef/Classes/MainController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class MainController
+    {
+        public static List<Recette> recettes;
+        public static List<Table> tables;
+        public static List<Materiel> materiel;
+        public static List<MaterielLavable> materielLavable;
+        ...
+
+        public static void remplirRecettes()
+        {
+            /*je me connecte a la base de donnees et je remplis la liste des recettes*/
+        }
+
+        public static void remplirMateriel()
+        {
+            /*je me connct a la base donnees et je remplis la liste du meteriel*/
+        }
+        ...
+    }
+}
